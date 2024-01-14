906aa777e7d6d804e83c88144f9c14989b1dea94
lehuy <lehuy.nguyen@ekino.com>
2024-01-14 16:48:13 +0700

    init(finished 03)

---
+++ routes/auth.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::middleware('guest')->group(function () {
+    Volt::route('register', 'pages.auth.register')
+        ->name('register');
+
+    Volt::route('login', 'pages.auth.login')
+        ->name('login');
+
+    Volt::route('forgot-password', 'pages.auth.forgot-password')
+        ->name('password.request');
+
+    Volt::route('reset-password/{token}', 'pages.auth.reset-password')
+        ->name('password.reset');
+});
+
+Route::middleware('auth')->group(function () {
+    Volt::route('verify-email', 'pages.auth.verify-email')
+        ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Volt::route('confirm-password', 'pages.auth.confirm-password')
+        ->name('password.confirm');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('auth/{provider}/redirect', [SocialiteController::class, 'loginSocial'])
+        ->name('socialite.auth');
+
+    Route::get('auth/{provider}/callback', [SocialiteController::class, 'callbackSocial'])
+        ->name('socialite.callback');
+});
